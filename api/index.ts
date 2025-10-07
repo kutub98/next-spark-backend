229e1bc25eafddcb5785e862a888f70bfa15082b
Kutub Udd <socialnetwork98710@gmail.com>
2025-10-07 12:14:20 +0600

    added

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
+import app from '../dist/app';
 import serverless from 'serverless-http';
-import app from '../dist/app'; // after build, dist/app.js exists
 
 const handler = serverless(app);
 export default handler;
+
 export const config = {
-  api: {
-    bodyParser: false,
-  },
+  api: { bodyParser: false },
 };
